40b90e77f1d9a055a1545ae84989fa290770ec24
Mikael <mikael.srodrigues@gmail.com>
2022-11-10 21:19:14 -0200

    ADO Aula 12
    Ado aula 12 não terminada

---
+++ AdoAula12/Quadrado.php
@@ -0,0 +1,18 @@
+<?php
+
+class Quadrado extends Retangulo {
+        
+        public function setLargura($largura) {
+                $this->m_largura = $largura;
+                $this->m_altura = $largura;
+        }
+
+        public function setAltura ($altura) {
+                $this->m_largura = $altura;
+                $this->m_altura = $altura;
+        } }
+
+        
+$q1 = new Quadrado;
+$q1->setAltura(10);
+$q1->setLargura(5); echo $q1->getArea(); 

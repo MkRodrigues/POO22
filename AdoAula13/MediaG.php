0950f1b0e9df1cfff4c7bd30274aea1ca78e0ba3
Mikael <mikael.srodrigues@gmail.com>
2022-11-11 22:20:03 -0300

    ADO 13
    Correção da ADO 13

--- AdoAula13/MediaG.php
+++ AdoAula13/MediaG.php
@@ -3,7 +3,6 @@
 require_once("IMediaGeometrica.php");
 require_once("IMedia.php");
 
-
 class MediaG implements IMediaGeometrica{
     private $aluno;
     private $nota1;
@@ -15,22 +14,8 @@ class MediaG implements IMediaGeometrica{
     }
     
     public function setDados ($nota1, $nota2, $aluno){
-            
+        $this->nota1 = $nota1;
+        $this->nota2 = $nota2;
+        $this->aluno = $aluno;   
     }
-
 }
-
-
-// $i = 0;
-        // foreach ( $notas as $n ) 
-        // {
-        //          $this->notas [ $i ] = $n;
-        //          $i++;
-        // }
-
-
-        // $m = new mediaG;
-        // $m->setDados ( 5, 6,"Aluno");
-        // $media = $m->calcularMediaGeometrica();
-        // echo $media; 
-
